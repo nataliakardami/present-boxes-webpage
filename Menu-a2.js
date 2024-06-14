c1e54a1712e5e0acf0758fdaf367a35796b1d1f6
“Natalia <natalia.kardami@ru.nl>
2024-06-14 15:13:29 +0900

    IMAGES SHOW UP

--- Menu-a2.js
+++ Menu-a2.js
@@ -7,9 +7,9 @@ class Menu {
       
       const unopened = "gift.jpeg";
       this.presents = [
-        new Present(this.src, 'gift.jpeg',this.showButtonClicked),
-        new Present(this.src, 'gift.jpeg', this.showButtonClicked),
-        new Present(this.src, 'gift.jpeg', this.showButtonClicked)
+        new Present(this.imgContainer, 'gift.jpeg',this.showButtonClicked),
+        new Present(this.imgContainer, 'gift.jpeg', this.showButtonClicked),
+        new Present(this.imgContainer, 'gift.jpeg', this.showButtonClicked)
       ];
       
       document.addEventListener('click', this.showButtonClicked);
@@ -20,15 +20,16 @@ class Menu {
     }
 }
   /*  Class Present  */
-  class Present {
+class Present {
     /**
      * Present container object
      * @param {} containerElement create a container for img
-     * @param {*} gifSrc the path to the image stc
+     * @param {*} src_dir the path to the image stc
      * @param {*} onClickedCallback callback param function
      */
-    constructor(containerElement, gifSrc, onClickedCallback) {
-      this.containerElement = containerElement;
+    constructor(containerElement, src_dir, onClickedCallback) {
+      /* 
+         this.containerElement = containerElement;
       this.gifSrc = gifSrc;
       this.onClickedCallback = onClickedCallback;
       
@@ -38,11 +39,38 @@ class Menu {
       image.src = 'gift.jpeg';
       button.addEventListener('click', this.onClick);
       this.containerElement.append(image);
-    }
+*/
+   
+
+      // some oop stuff
+      this.containerElement = containerElement;
+      this.src_dir = src_dir;
+      this.onClickedCallback = onClickedCallback;
+      this.onClick = this.onClick.bind(this);
+
+      // create img and allocate src dir
+      const image = document.createElement('img');
+      src_dir = "img/gift.jpeg"; //TODO #1 :  change this
+      image.src = src_dir;
+      image.addEventListener('click', () => {
+          this._openPresent(src_dir);
+      });
+
+      this.containerElement.append(image);
+
+
+  }
+  _openPresent(event, source){
+      const image = event.currentTarget;
+      image.src = source || "img/cat1.gif";
+      image.removeEventListener('click', this._openPresent);
+      //event.currentTarget.src = event.currentTarget.src == "img/cat1.gif" ? "img/gift.jpeg" : "img/cat1.gif";
+  }
+  
     
     onClick() {
         this.onClickedCallback(this.gifSrc);
     }
+  
   }
-
-  new Menu();
+new Menu();
