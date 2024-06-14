503a372a4cea2bcc23929119802dd3d1087c78e7
“Natalia <natalia.kardami@ru.nl>
2024-06-14 16:36:21 +0900

    .js added comments, .css flex

--- Menu-a2.js
+++ Menu-a2.js
@@ -2,26 +2,30 @@
 const GIF_LIST = [
     "img/cat1.gif",
     "img/cat2.gif",
+    "img/cat3.gif",
     "img/cat4.gif",
+    "img/cat5.gif"
 ]
 class Menu {
     constructor() {
       this.imgContainer = document.querySelector('#menu');
       this.statusBar = document.querySelector('#status-bar');
       
-      this.showButtonClicked = this.showButtonClicked.bind(this);
+      this.allImgClicked = this.allImgClicked.bind(this);
       
       const unopened = "gift.jpeg";
-      this.presents = [
-        new Present(this.imgContainer, 'gift.jpeg',this.showButtonClicked,0),
-        new Present(this.imgContainer, 'gift.jpeg', this.showButtonClicked,1),
-        new Present(this.imgContainer, 'gift.jpeg', this.showButtonClicked,2)
-      ];
-      
-      document.addEventListener('click', this.showButtonClicked);
+      this.presents = [];
+
+      // create a present object for each image in GIF_LIST and append it to the presents array
+      for (let i=0; i<GIF_LIST.length; i++){
+        this.presents.push(new Present(this.imgContainer, unopened, this.allImgClicked, i));
+      }
+
+      // add event listener to document to check if all the images have been clicked
+      document.addEventListener('click', this.allImgClicked);
     }
     
-    showButtonClicked() {
+    allImgClicked() {
       // If all images are clicked, then the array of images will be full of "-1"
       if (GIF_LIST.every((element) => element === "-1")){
         this.statusBar.textContent = "Enjoy your presents!";
@@ -50,6 +54,7 @@ class Present {
       const image = document.createElement('img');
       image.src = src_dir; // this works
 
+      // add event listener and append the image to the container element
       image.addEventListener('click', this.onClick);
       this.containerElement.appendChild(image); 
 
@@ -59,12 +64,13 @@ class Present {
      * @param {*} event 
      */
     onClick(event){
-      // call the callback function
+        // call the callback function
         const image = event.currentTarget;
         image.src = GIF_LIST[this.index];
+        // im not even sure if the callback function is needed here
         this.onClickedCallback(this.index);
 
-        // change src to flag index -1, notifing that the image has been clicked
+        // change src to flag index -1, notifying that the image has been clicked
         GIF_LIST[this.index] = "-1";
         console.log(GIF_LIST);
         image.removeEventListener('click', this.onClick);
